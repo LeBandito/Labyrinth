9ef7a1289155981f3d9a8ea4c3f9b863167b1276
Slonik <polslonika@gmail.com>
2026-07-03 14:22:17 +0300

    v2: небольшие корректировки

--- a1.cpp
+++ a1.cpp
@@ -28,38 +28,38 @@ void CreateStartFinish(const int& vidth, const int& height, int& start_x, int& s
     
     // start
     if ((vidth / start) && true) {
-        start_x = start;
-        start_y = 1;
+        start_x = start - 1;
+        start_y = 0;
     } else if (((vidth + height) / start) && true) {
-        start_x = vidth;
-        start_y = start - vidth;
+        start_x = vidth - 1;
+        start_y = start - vidth - 1;
     } else if (((vidth + height + vidth) / start) && true) {
-        start_x = vidth - (start - (vidth + height)) + 1;
-        start_y = height;
+        start_x = vidth - (start - (vidth + height));
+        start_y = height - 1;
     } else if (((vidth + height + vidth + height) / start) && true) {
-        start_x = 1;
-        start_y = (vidth + height + vidth + height) - start;
+        start_x = 0;
+        start_y = (vidth + height + vidth + height) - start - 1;
     }
     
-    std::cout << " -> (" << start_x << "; " << start_y << ")" << std::endl;
+    std::cout << " -> (" << start_x + 1<< "; " << start_y + 1<< ")" << std::endl;
     
     std::cout << "finish = " << finish;
     // finish
     if ((vidth / finish) && true) {
-        finish_x = finish;
-        finish_y = 1;
+        finish_x = finish - 1;
+        finish_y = 0;
     } else if (((vidth + height) / finish) && true) {
-        finish_x = vidth;
-        finish_y = finish - vidth;
+        finish_x = vidth - 1;
+        finish_y = finish - vidth - 1;
     } else if (((vidth + height + vidth) / finish) && true) {
-        finish_x = vidth - (finish - (vidth + height)) + 1;
-        finish_y = height;
+        finish_x = vidth - (finish - (vidth + height));
+        finish_y = height - 1;
     } else if (((vidth + height + vidth + height) / finish) && true) {
-        finish_x = 1;
-        finish_y = (vidth + height + vidth + height) - finish;
+        finish_x = 0;
+        finish_y = (vidth + height + vidth + height) - finish - 1;
     }
     
-    std::cout << " -> (" << finish_x << "; " << finish_y << ")" << std::endl;
+    std::cout << " -> (" << finish_x + 1<< "; " << finish_y + 1 << ")" << std::endl;
 }
 
 void TemperaryFind(const int& x, const int& y, std::pair<int, int> temporary_xy) {
@@ -81,34 +81,44 @@ void CreateMoving(std::vector<std::pair<int, int>>& WinRoute, std::vector<std::v
     ShowLabirint(labirint);
     
     std::cout << "Create ROADS!" << std::endl;
-    
-    // Старая идея - 1 проход: случайные точки; 2 проход: пытаемся соединить случайные точки
-    
+    // Новая идея - 1 проход: случайные точки; 2 проход: случайные УГЛОВЫЕ точки 
     // Второй проход - соединяем точки в единый маршрут
     // Cлева
-    if (start_x == 1) {
-        labirint[start_y - 1][WinRoute[0].first] = 'A';
+    if (start_x == 0) {
+        labirint[start_y][WinRoute[0].first] = 'A';
         // До поворота
         for (int i = 1; i < WinRoute[0].first; ++i)
-            labirint[start_y - 1][i] = '1';
+            labirint[start_y][i] = '1';
         // До точки
-        for (int i = WinRoute[0].second; i < WinRoute[0].first; ++i)
-            labirint[start_y - 1][i] = '1';
+        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
+            labirint[i][WinRoute[0].first] = '2';
     // Справа
-    } else if (start_x == vidth) {
-        labirint[start_y - 1][WinRoute[0].first] = 'B';
-        for (int i = WinRoute[0].first + 1; i < start_x - 1; ++i)
-            labirint[start_y - 1][i] = '2';
+    } else if (start_x == vidth - 1) {
+        labirint[start_y][WinRoute[0].first] = 'B';
+        // До поворота
+        for (int i = WinRoute[0].first + 1; i < start_x; ++i)
+            labirint[start_y][i] = '3';
+        // До точки
+        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
+            labirint[i][WinRoute[0].first] = '4';
     // Сверху
-    } else if (start_y == 1) {
-        labirint[WinRoute[0].second][start_x - 1] = 'C';
+    } else if (start_y == 0) {
+        labirint[WinRoute[0].second][start_x] = 'C';
+        // До поворота
         for (int i = 1; i < WinRoute[0].second; ++i)
-            labirint[i][start_x - 1] = '3';
+            labirint[i][start_x] = '5';
+        // До точки
+        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
+            labirint[i][WinRoute[0].first] = '6';
     // Снизу
-    } else if (start_y == height) {
-        labirint[WinRoute[0].second][start_x - 1] = 'D';
-        for (int i = WinRoute[0].second + 1; i < start_y - 1; ++i)
-            labirint[i][start_x - 1] = '4';
+    } else if (start_y == height - 1) {
+        labirint[WinRoute[0].second][start_x] = 'D';
+        // До поворота
+        for (int i = WinRoute[0].second + 1; i < start_y; ++i)
+            labirint[i][start_x] = '7';
+        // До точки
+        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
+            labirint[i][WinRoute[0].first] = '8';
     }
     
     // for (int i = 1; i < WinRoute.size(); ++i) {
@@ -135,8 +145,8 @@ int main() {
     int start_x(0), start_y(0);
     int finish_x(0), finish_y(0);
     CreateStartFinish(vidth, height, start_x, start_y, finish_x, finish_y);
-    labirint[start_y - 1][start_x - 1] = 'S';
-    labirint[finish_y - 1][finish_x - 1] = 'F';
+    labirint[start_y][start_x] = 'S';
+    labirint[finish_y][finish_x] = 'F';
     ShowLabirint(labirint);
     
     std::vector<std::pair<int, int>> WinRoute;
