c38ceb78f72ceedf03d1b5db493b77bff5bc25d3
Slonik <polslonika@gmail.com>
2026-07-03 14:40:25 +0300

    v2.1: Немного сократил решение

--- a1.cpp
+++ a1.cpp
@@ -21,56 +21,93 @@ void ShowLabirint(const std::vector<std::vector<char>>& labirint) {
     std::cout << std::endl;
 }
 
-void CreateStartFinish(const int& vidth, const int& height, int& start_x, int& start_y, int& finish_x, int& finish_y) {
-    int start = 0 + std::rand() % (2 * (vidth + height)); 
-    int finish = 0 + std::rand() % (2 * (vidth + height)); 
-    std::cout << "start = " << start;
-    
-    // start
-    if ((vidth / start) && true) {
-        start_x = start - 1;
-        start_y = 0;
-    } else if (((vidth + height) / start) && true) {
-        start_x = vidth - 1;
-        start_y = start - vidth - 1;
-    } else if (((vidth + height + vidth) / start) && true) {
-        start_x = vidth - (start - (vidth + height));
-        start_y = height - 1;
-    } else if (((vidth + height + vidth + height) / start) && true) {
-        start_x = 0;
-        start_y = (vidth + height + vidth + height) - start - 1;
+void Create_X_Y(const int& vidth, const int& height, const int& need, int& need_x, int& need_y) {
+    if ((vidth / need) && true) {
+        need_x = need - 1;
+        need_y = 0;
+    } else if (((vidth + height) / need) && true) {
+        need_x = vidth - 1;
+        need_y = need - vidth - 1;
+    } else if (((vidth + height + vidth) / need) && true) {
+        need_x = vidth - (need - (vidth + height));
+        need_y = height - 1;
+    } else if (((vidth + height + vidth + height) / need) && true) {
+        need_x = 0;
+        need_y = (vidth + height + vidth + height) - need - 1;
     }
+}
+
+void CreateStartFinish(const int& vidth, const int& height, int& start_x, int& start_y, int& finish_x, int& finish_y) {
+    int start(0);
+    do {
+       start = std::rand() % (2 * (vidth + height)); 
+    } while ((start == 0) || (start == vidth) || (start == vidth + height) || (start == vidth + height + vidth));
     
+    int finish(0);
+    do {
+        finish = std::rand() % (2 * (vidth + height)); 
+    } while ((finish == 0) || (finish == vidth) || (finish == vidth + height) || (finish == vidth + height + vidth));
+     
+    std::cout << "start = " << start;
+    Create_X_Y(vidth, height, start, start_x, start_y);
     std::cout << " -> (" << start_x + 1<< "; " << start_y + 1<< ")" << std::endl;
     
     std::cout << "finish = " << finish;
-    // finish
-    if ((vidth / finish) && true) {
-        finish_x = finish - 1;
-        finish_y = 0;
-    } else if (((vidth + height) / finish) && true) {
-        finish_x = vidth - 1;
-        finish_y = finish - vidth - 1;
-    } else if (((vidth + height + vidth) / finish) && true) {
-        finish_x = vidth - (finish - (vidth + height));
-        finish_y = height - 1;
-    } else if (((vidth + height + vidth + height) / finish) && true) {
-        finish_x = 0;
-        finish_y = (vidth + height + vidth + height) - finish - 1;
-    }
-    
+    Create_X_Y(vidth, height, finish, finish_x, finish_y);
     std::cout << " -> (" << finish_x + 1<< "; " << finish_y + 1 << ")" << std::endl;
 }
 
-void TemperaryFind(const int& x, const int& y, std::pair<int, int> temporary_xy) {
-    
+void StaticRoutes(std::vector<std::vector<char>>& labirint, const int& vidth, const int& height, 
+    const int& Win_x, const int& Win_y, const int& need_x, const int& need_y) {
+    // Win - точка, в которую идем. need - точка, из которой идем
+    // По горизонтали
+    if ((need_x == 0) || (need_x == vidth - 1)) {
+        labirint[need_y][Win_x] = 'A';
+        // До поворота
+        for (int i = std::min(Win_x, need_x) + 1; i < std::max(Win_x, need_x); ++i)
+            labirint[need_y][i] = '1';
+        // До точки
+        for (int i = std::min(Win_y, need_y) + 1; i < std::max(Win_y, need_y); ++i)
+            labirint[i][Win_x] = '2';
+    // По вертикали
+    } else if ((need_y == 0) || (need_y == height - 1)) {
+        labirint[Win_y][need_x] = 'C';
+        // До поворота
+        for (int i = std::min(Win_y, need_y) + 1; i < std::max(Win_y, need_y); ++i)
+            labirint[i][need_x] = '3';
+        // До точки
+        for (int i = std::min(Win_x, need_x) + 1; i < std::max(Win_x, need_x); ++i)
+            labirint[Win_y][i] = '4';
+    }
+}
+
+void RandomMoving(std::vector<std::vector<char>>& labirint, const int& vidth, const int& height, 
+    const int& Win_x, const int& Win_y, const int& need_x, const int& need_y) {
+    // Win - точка, в которую идем. need - точка, из которой идем
+    // Выбираем случайно
+    if (std::rand() & 1) {
+        labirint[Win_y][need_x] = 't';
+        // До поворота
+        for (int i = std::min(Win_y, need_y) + 1; i < std::max(Win_y, need_y); ++i)
+            labirint[i][need_x] = '6';
+        // До точки
+        for (int i = std::min(Win_x, need_x) + 1; i < std::max(Win_x, need_x); ++i)
+            labirint[Win_y][i] = '7';
+    } else {
+        labirint[need_y][Win_x] = 'f';
+        // До поворота
+        for (int i = std::min(Win_x, need_x) + 1; i < std::max(Win_x, need_x); ++i)
+            labirint[need_y][i] = '8';
+        // До точки
+        for (int i = std::min(Win_y, need_y) + 1; i < std::max(Win_y, need_y); ++i)
+            labirint[i][Win_x] = '9';
+    }
 }
 
 void CreateMoving(std::vector<std::pair<int, int>>& WinRoute, std::vector<std::vector<char>>& labirint, const int& vidth, const int& height, 
     const int& start_x, const int& start_y, const int& finish_x, const int& finish_y) {
     int count = ((vidth - 1) + (height - 1)) / 5;
-    
-    std::pair<int, int> temporary_xy(0, 0); 
+    std::pair<int, int> temporary_xy(0, 0);
     
     // Первый проход - назначем точки победных поворотов
     for (int i = 0; i < count; ++i) {
@@ -80,57 +117,25 @@ void CreateMoving(std::vector<std::pair<int, int>>& WinRoute, std::vector<std::v
     }
     ShowLabirint(labirint);
     
-    std::cout << "Create ROADS!" << std::endl;
+    std::cout << "Create First ROADS!" << std::endl;
     // Новая идея - 1 проход: случайные точки; 2 проход: случайные УГЛОВЫЕ точки 
     // Второй проход - соединяем точки в единый маршрут
-    // Cлева
-    if (start_x == 0) {
-        labirint[start_y][WinRoute[0].first] = 'A';
-        // До поворота
-        for (int i = 1; i < WinRoute[0].first; ++i)
-            labirint[start_y][i] = '1';
-        // До точки
-        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
-            labirint[i][WinRoute[0].first] = '2';
-    // Справа
-    } else if (start_x == vidth - 1) {
-        labirint[start_y][WinRoute[0].first] = 'B';
-        // До поворота
-        for (int i = WinRoute[0].first + 1; i < start_x; ++i)
-            labirint[start_y][i] = '3';
-        // До точки
-        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
-            labirint[i][WinRoute[0].first] = '4';
-    // Сверху
-    } else if (start_y == 0) {
-        labirint[WinRoute[0].second][start_x] = 'C';
-        // До поворота
-        for (int i = 1; i < WinRoute[0].second; ++i)
-            labirint[i][start_x] = '5';
-        // До точки
-        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
-            labirint[i][WinRoute[0].first] = '6';
-    // Снизу
-    } else if (start_y == height - 1) {
-        labirint[WinRoute[0].second][start_x] = 'D';
-        // До поворота
-        for (int i = WinRoute[0].second + 1; i < start_y; ++i)
-            labirint[i][start_x] = '7';
-        // До точки
-        for (int i = std::min(WinRoute[0].second, start_y) + 1; i < std::max(WinRoute[0].second, start_y); ++i)
-            labirint[i][WinRoute[0].first] = '8';
-    }
     
-    // for (int i = 1; i < WinRoute.size(); ++i) {
-    //     if () {
-            
-    //     }
-    // }
-    
+    // По горизонтали
+    StaticRoutes(labirint, vidth, height, WinRoute[0].first, WinRoute[0].second, start_x, start_y);
+    // По вертикали
+    StaticRoutes(labirint, vidth, height, WinRoute[WinRoute.size() - 1].first, WinRoute[WinRoute.size() - 1].second, finish_x, finish_y);
     ShowLabirint(labirint);
     
+    std::cout << "Create Second ROADS!" << std::endl;
+    for (int i = 0; i < WinRoute.size() - 1; ++i) {
+        RandomMoving(labirint, vidth, height, WinRoute[i + 1].first, WinRoute[i + 1].second, WinRoute[i].first, WinRoute[i].second);
+        // std::cout << i + 1 << ") " << std::endl;
+        // ShowLabirint(labirint);
+    }
     
-    
+    ShowLabirint(labirint);
+
 }
 
 int main() {
@@ -145,7 +150,9 @@ int main() {
     int start_x(0), start_y(0);
     int finish_x(0), finish_y(0);
     CreateStartFinish(vidth, height, start_x, start_y, finish_x, finish_y);
+    // S = Start
     labirint[start_y][start_x] = 'S';
+    // F = Finish
     labirint[finish_y][finish_x] = 'F';
     ShowLabirint(labirint);
     
